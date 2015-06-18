7b6dbe810810f3d6a83ecdd2e7ba0e8739c27019
Tericky <tericky@gmail.com>
2015-06-19 00:37:11 +0800

    重寫顯示，加入 kml & spot 讀取顯示

---
+++ js/data.js
@@ -0,0 +1,124 @@
+var routeDatas = 
+[
+	{
+		Id:1,
+		Name:'中彰投雲分段',
+		Route:3,
+		Spot:
+		[
+			 {
+		        mid: 'marker1_1',
+		        LatLng : [24.078512,120.543553],
+		        iconColor : 'blue-dot',
+		        title: '彰化孔子廟',
+		        infoContent : { Class: 'primary', Header:'彰化孔子廟', Body:'<div class="StreetView" />', Footer : '500台灣彰化縣彰化市孔門路30號'}
+		    },
+			{
+		        mid: 'marker1_2',
+		        LatLng : [24.081372,120.540162],
+		        iconColor : 'blue-dot',
+		        title: '八卦山大佛風景區',
+		        infoContent : { Class: 'primary', Header:'八卦山大佛風景區', Body:'<div class="StreetView" />', Footer : '500台灣彰化縣彰化市溫泉路31號'}
+		    },
+			{
+		        mid: 'marker1_3',
+		        LatLng : [23.830884,120.475682],
+		        iconColor : 'blue-dot',
+		        title: '溪州花博公園',
+		        infoContent : { Class: 'primary', Header:'溪州花博公園', Body:'<div class="StreetView" />', Footer : '台灣彰化縣溪州鄉'}
+		    },
+			{
+		        mid: 'marker1_4',
+		        LatLng : [23.8506537,120.9167357],
+		        iconColor : 'blue-dot',
+		        title: '日月潭',
+		        infoContent : { Class: 'primary', Header:'日月潭', Body:'<div class="StreetView" />', Footer : '555中華民國南投縣魚池鄉'}
+		    },
+			{
+		        mid: 'marker1_5',
+		        LatLng : [23.868565,120.95908],
+		        iconColor : 'blue-dot',
+		        title: '九族文化村',
+		        infoContent : { Class: 'primary', Header:'九族文化村', Body:'<div class="StreetView" />', Footer : '555台灣南投縣魚池鄉金天巷45號'}
+		    },
+			{
+		        mid: 'marker1_6',
+		        LatLng : [23.960679,120.974701],
+		        iconColor : 'blue-dot',
+		        title: '國立暨南國際大學',
+		        infoContent : { Class: 'primary', Header:'國立暨南國際大學', Body:'<div class="StreetView" />', Footer : '台灣南投縣埔里鎮大學路1號'}
+		    },
+			{
+		        mid: 'marker1_7',
+		        LatLng : [23.960679,120.974701],
+		        iconColor : 'blue-dot',
+		        title: '埔里酒廠',
+		        infoContent : { Class: 'primary', Header:'埔里酒廠', Body:'<div class="StreetView" />', Footer : '545台灣南投縣埔里鎮中山路三段219號'}
+		    },
+			{
+		        mid: 'marker1_8',
+		        LatLng : [23.994401,120.724419],
+		        iconColor : 'blue-dot',
+		        title: '寶島時代村',
+		        infoContent : { Class: 'primary', Header:'寶島時代村', Body:'<div class="StreetView" />', Footer : '542台灣南投縣草屯鎮中正路1039號'}
+		    }
+		],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('中彰投雲分段') + '.kml'
+	},
+	{
+		Id:2,
+		Name:'宜蘭東北角分段',
+		Route:2,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('宜蘭東北角分段') + '.kml'
+	},
+	{
+		Id:3,
+		Name:'花蓮-玉里分段',
+		Route:3,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('花蓮-玉里分段') + '.kml'
+	},
+	{
+		Id:4,
+		Name:'高屏臺東分段',
+		Route:5,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('高屏臺東分段') + '.kml'
+	},
+	{
+		Id:5,
+		Name:'嘉南分段',
+		Route:3,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('嘉南分段') + '.kml'
+	},
+	{
+		Id:6,
+		Name:'臺北-臺中分段',
+		Route:4,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('臺北-臺中分段') + '.kml'
+	},
+	{
+		Id:7,
+		Name:'臺東-玉里分段',
+		Route:10,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('臺東-玉里分段') + '.kml'
+	},
+	{
+		Id:8,
+		Name:'環島9天行程規劃',
+		Route:10,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('環島9天行程規劃') + '.kml'
+	},
+	{
+		Id:9,
+		Name:'環島順道規劃',
+		Route:2,
+		Spot:[],
+		Url:'https://raw.githubusercontent.com/tericky/Bike.Taiwan.Demo/master/data/' + encodeURIComponent('環島順道規劃.kml') + '.kml'
+	}
+]
